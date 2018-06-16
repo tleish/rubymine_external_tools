baabf5d79bc8820bfd5b0b46254a4f7033ed44b7
Thomas Leishman <tleishman@plansource.com>
2018-06-16 11:05:24 -0600

    updates

--- rubocop.sh
+++ rubocop.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 . ~/.profile
-echo '==============='
-echo
-echo "> rubocop $1"
-rubocop $1
-echo
+# cd ~ # change directory so project Gemfile doesn't interfere
+set -x # START: output shell commands to the script
+rubocop --config ~/.rubocop.yml $1
+# set +x # STOP: output shell commands to the script
